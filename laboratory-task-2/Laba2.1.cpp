83e9856cf73aecbbaf5ecdd2b27382bddeabd89e
NikitaKonturov <nikitakonturov@gmail.com>
2023-11-28 00:05:29 +0300

    Реализовал быстрое возведение в степнь, поправил названия переменных

--- laboratory-task-2/Laba2.1.cpp
+++ laboratory-task-2/Laba2.1.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
-uint64_t quickPow(const int& a, const int& b) {
-	uint64_t res = static_cast<uint64_t>(a);
-	for (int i = 1; i < b; ++i) {
-		res *= static_cast<uint64_t>(a);
-	}
-	return res;
+
+
+uint64_t quickPow(int a, int b) {
+        if (b == 0){
+                return 1;
+        }
+        if (b % 2 == 0){
+                return quickPow(a * a, b / 2);
+        }else{
+                return a * quickPow(a, b - 1);
+        }
 }
 
 
-void main() {
-	uint64_t num = 0;
+uint64_t getNum(){
+	int64_t num = 0;
 	while (num <= 0){
 		std::cout << "Enter num = ";
 		std::cin >> num;
 		std::cout << '\n';
 	}
-	uint64_t num_of_numbers = 0;
-	int temp = num;
-	while (temp != 0) {
-		temp /= 10;
-		num_of_numbers++;
+	return static_cast<uint64_t>(num);
+}
+
+
+uint64_t getNumOfNumbers(uint64_t  num){
+	uint64_t numOfNumbers = 0;
+	while(num != 0){
+		num /= 10;
+		++numOfNumbers;
 	}
+	return numOfNumbers;
+}
 
-	std::cout << "num_of_numbers = " << num_of_numbers << '\n';
+int main() {
+	uint64_t num =getNum();
+	uint64_t numOfNumbers = getNumOfNumbers(num);
 
-	if ((num_of_numbers % 2) != 0) {
-		uint64_t central_num = num / powMy(10, ((num_of_numbers + 1) / 2) - 1) % 10;
+	std::cout << "num_of_numbers = " << numOfNumbers << '\n';
+
+	if ((numOfNumbers % 2) != 0) {
+		uint64_t central_num = num / quickPow(10, ((numOfNumbers + 1) / 2) - 1) % 10;
 		std::cout << "central_num = " << central_num << '\n';
-		uint64_t result = central_num * powMy(10, num_of_numbers + 1) + num * 10 + central_num;
+		uint64_t result = central_num * quickPow(10, numOfNumbers + 1) + num * 10 + central_num;
 		std::cout << "Result = " << result;
 	}
 	else {
 		std::cout << "It's even...";
 	}
-
+	return 0;
 }
