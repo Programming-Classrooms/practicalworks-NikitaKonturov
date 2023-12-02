d32662e8c8c0fde0ca7be9064b0c028d88426fac
NikitaKonturov <nikitakonturov@gmail.com>
2023-12-02 21:45:42 +0300

    Сделал передачу файлов в функцию и уже там создание потоков добавил закрытие файлов

--- practical-works/WorkWithFies.cpp
+++ practical-works/WorkWithFies.cpp
@@ -9,28 +9,39 @@ void checkFile(std::ifstream &file){
     if (!file) throw "File not founded";
     if (!file.is_open()) throw "File unopened";
     if (file.eof()) throw "File is epmty";
+    std::string text;
+    std::string line;
+    while(!file.eof()){
+        std::getline(file, line);
+        text += line; 
+    }
+    if (text.size() == 0) throw "File is epmty";
 }
 
 
-void copyFiles(std::ifstream &file, std::ofstream &newFile){
+void copyFiles(const std::string pathForCopy, const std::string pathToCopy){
+    try{
+    std::ifstream file (pathForCopy);
     checkFile(file);
+    std::ofstream newFile(pathToCopy);
+    if (!newFile.is_open()) throw "File WorkWithFiles.txt unopened";
     std::string text, line;
     while(!file.eof()){
         std::getline(file ,line);
         text += line + '\n';
     }
+    file.close();
     newFile << text;
+    newFile.close();
+    }catch(std::exception &err){
+        std::cerr << "Exeption: "<< err.what() << std::endl;
+    }
 }
 
 
 int main(){
-    try{
-    const std::string path = "/home/nikita/Desktop/FolderByWorcsk/WorkWithFies.cpp";
-    std::ifstream file (path);
-    std::ofstream newFile("WorkWithFiles.txt");
-    copyFiles(file, newFile);
-    }catch(std::exception &err){
-        std::cerr << "Exeption: "<< err.what() << std::endl;
-    } 
+    const std::string pathForCopy = "/home/nikita/Desktop/FolderByWorcsk/WorkWithFies.cpp";
+    const std::string pathToCopy = "WorkWithFiles.txt";
+    copyFiles(pathForCopy, pathToCopy);
     return 0;
 }
