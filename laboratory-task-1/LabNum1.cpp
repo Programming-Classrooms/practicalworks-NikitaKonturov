bcfeb0aca72d9be10a3198f4fa481639e6577a55
NikitaKonturov <nikitakonturov@gmail.com>
2023-12-13 02:14:41 +0300

    Поменял название пременных, добавил задание

--- laboratory-task-1/LabNum1.cpp
+++ laboratory-task-1/LabNum1.cpp
@@ -1,33 +1,38 @@
+/*Определить к какому интерввалу принадлежит х,
+границы интерваллов задаёт пользователь.*/
+
 #include <iostream>
 #include <string>
 #include <exception>
+#include <Windows.h>
 
 
 void main() {
 	try {
-		setlocale(0, "");
+		SetConsoleCP(1225);
+		SetConsoleOutputCP(1225);
 		std::cout << "Enter borders valeus = ";
-		double a, b;
+		double leftBoard, rightBoard;
 		std::string aStr, bStr;
 		std::cin >> aStr >> bStr;
-		a = std::stod(aStr);
-		b = std::stod(bStr);
-		if (b < a) {
-			std::swap(a, b);
+		leftBoard = std::stod(aStr);
+		rightBoard = std::stod(bStr);
+		if (rightBoard < leftBoard) {
+			std::swap(leftBoard, rightBoard);
 		}
 		double x;
 		std::string xStr;
 		std::cout << "Enter x = ";
 		std::cin >> xStr;
 		x = std::stod(xStr);
-		if (a < x && b > x) {
-			std::cout << "x find in interval (" << a << ',' << b << ')';
+		if (leftBoard < x && rightBoard > x) {
+			std::cout << "x find in interval (" << leftBoard << ',' << rightBoard << ')';
 		}
-		else if (x < a) {
-			std::cout << "x find in interval ( " << x << ',' << "<" << a << ')';
+		else if (x < leftBoard) {
+			std::cout << "x find in interval ( " << x << ',' << "<" << leftBoard << ')';
 		}
-		else if (x > b) {
-			std::cout << "x find in interval ( " << x << ',' << ">" << b << ')';
+		else if (x > rightBoard) {
+			std::cout << "x find in interval ( " << x << ',' << ">" << rightBoard << ')';
 		}
 	}
 	catch (std::invalid_argument) {
