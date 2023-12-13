333bab75dbc0118e7f9e5c23bf48e07a69b92519
NikitaKonturov <nikitakonturov@gmail.com>
2023-12-13 16:01:53 +0300

    Поменял названия некоторых переменных, добавил проверку строки на полную заполненность пробелами

--- laboratory-task-7/worckWithLines.cpp
+++ laboratory-task-7/worckWithLines.cpp
@@ -5,32 +5,48 @@
 #include <exception>
  
 
+ bool checkAllSpace(char* text){
+    for (; *text != '\0'; ++text){
+        if(*text != ' ') return false;
+    }
+    throw std::exception("The text must contain at least one character");
+ }
+
+
 void getText(char* text,const char* textToWrite) {
     std::cout << "Please enter " << textToWrite << std::endl;
     std::cin.getline(text, 300);
-    if (std::strlen(text) == 0) throw std::exception("Error line is empty...");
+    if (std::strlen(text) == 0) {
+        throw std::exception("Error line is empty...");
+    }
 }
 
 
-bool IsPalindrom(char* word) {
+bool isPalindrom(char* word) {
     size_t lenWord = strlen(word);
     for (size_t i = 0; i < lenWord / 2; ++i) {
-        if (word[i] != word[lenWord - 1 - i]) return false;
+        if (word[i] != word[lenWord - 1 - i]) {
+            return false;
+        }
     }
     return true;
 }
 
 
 size_t seekMaxLenPolindrom(char* text, char* delimiters) {
-    char saveText[300];
-    std::strcpy(saveText, text);
-    char* word = strtok(saveText, delimiters);
-    size_t maxLen = 0;
-    while (word != NULL) {
-        if (maxLen < std::strlen(word) && IsPalindrom(word)) maxLen = std::strlen(word);
-        word = strtok(NULL, delimiters);
-    }
+    if (!checkAllSpace(text)){
+        char saveText[300];
+        std::strcpy(saveText, text);
+        char* word = strtok(saveText, delimiters);
+        size_t maxLen = 0;
+        while (word != NULL) {
+            if (maxLen < std::strlen(word) && isPalindrom(word)) {
+                 maxLen = std::strlen(word);
+            }
+            word = strtok(NULL, delimiters);
+        }
     return maxLen;
+    }
 }
 
 
@@ -38,7 +54,7 @@ void printMaxPolindroms(char* text, char* delimiters, char* newLine) {
     size_t maxLenPolindrom = seekMaxLenPolindrom(text, delimiters);
     char* word = strtok(text, delimiters);
     while (word != NULL) {
-        if (IsPalindrom(word) && maxLenPolindrom == std::strlen(word)) {
+        if (isPalindrom(word) && maxLenPolindrom == std::strlen(word)) {
             strcat(newLine, word);
             strcat(newLine, ";");
         }
--- laboratory-task-7/worckWithLines.hpp
+++ laboratory-task-7/worckWithLines.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+
 void getText(char* text,const char* textToWrite);
-bool IsPalindrom(char* word);
+bool isPalindrom(char* word);
 void printMaxPolindroms(char* text, char* delimiters, char* newLine);
 int seekMaxLenPolindrom(char* text, char* delimiters);
+bool checkAllSpace(char* text);
