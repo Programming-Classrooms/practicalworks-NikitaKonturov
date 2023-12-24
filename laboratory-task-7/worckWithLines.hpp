00dc4f21338eb2b1318a2a6607090a08de6ed5f3
NikitaKonturov <nikitakonturov@gmail.com>
2023-12-24 18:23:16 +0300

    Исправил опечатку в типе функции

--- laboratory-task-7/worckWithLines.hpp
+++ laboratory-task-7/worckWithLines.hpp
@@ -5,7 +5,7 @@
 void getText(char* text,const char* textToWrite);
 bool isPalindrom(char* word);
 void printMaxPolindroms(char* text, char* delimiters, char* newLine);
-int seekMaxLenPolindrom(char* text, char* delimiters);
+size_t seekMaxLenPolindrom(char* text, char* delimiters);
 bool checkAllSpace(char* text);
 
 #endif
