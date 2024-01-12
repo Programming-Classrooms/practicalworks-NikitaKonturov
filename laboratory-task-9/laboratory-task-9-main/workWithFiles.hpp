c44dc2dc8358aeb27291fa3175970f2d2650c464
NikitaKonturov <nikitakonturov@gmail.com>
2024-01-12 23:13:39 +0300

    Убрал лишние файлы

--- laboratory-task-9/laboratory-task-9-main/workWithFiles.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-
-bool checkFile(std::ifstream& file);
-std::string getMainTextFromFile(const std::string path);
-std::string seekLinnesWithMaxNumberWords(std::string textWithAllLines);
-size_t countWords(std::string line);
