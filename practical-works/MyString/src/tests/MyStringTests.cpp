d32b06cd01152e2dc02bd0bcd3cb9555deec2d8c
¨NikitaKonturov¨ <nikitakonturov@gmail.com>
2024-02-21 18:56:26 +0300

    Убрал из проекта .gitignor

--- practical-works/MyString/src/tests/MyStringTests.cpp
+++ practical-works/MyString/src/tests/MyStringTests.cpp
@@ -20,11 +20,29 @@ TEST(testCaseConstructors, testCopyConstructor)
 {
     myString str1 = "Hello";
     myString str2(str1);
-    ASSERT_EQ(str1.getSize(), str2.getSize());
+    EXPECT_EQ(str1.getSize(), str2.getSize());
     for (size_t i = 0; i < str1.getSize(); ++i) {
         EXPECT_EQ(str1[i], str2[i]);
     }
 }
+
+TEST(testCasePushBack, testPushBack)
+{
+    myString str = "Hello";
+    str.push_back('!');
+    EXPECT_EQ(str[str.getSize() - 1], '!');
+}
+
+TEST(testCaseAppend, testAppendChar)
+{
+    myString str = "Hello";
+    str.append(" world");
+    char checkStr[] = "Hello world";
+    for (size_t i = 0; i < str.getSize(); ++i) {
+        EXPECT_EQ(str[i], checkStr[i]);
+    }
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
