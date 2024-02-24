fcb5a14d9948c55263c5a08529a898e5496b9a2e
¨NikitaKonturov¨ <nikitakonturov@gmail.com>
2024-02-24 23:25:50 +0300

    Добавил clean code + несколько методов

--- practical-works/MyString/src/funcs/myString.hpp
+++ practical-works/MyString/src/funcs/myString.hpp
@@ -2,43 +2,91 @@
 #define MYSTRING_HPP
 #include <cstring>
 #include <iostream>
+#include <math.h>
 
-class myString {
+
+class myString 
+{
 private:
-  char *str;
-  size_t size;
-  size_t capacity;
+    char *pStr;
+    size_t size;
+    size_t capacity;
 
 public:
-  myString();
-  myString(int64_t);
-  myString(const myString &);
-  myString(myString &&);
-  myString(const char *);
-  ~myString();
-  void push_back(char);
-  void append(const myString &);
-  void append(char *);
-  void append(const char *);
-  void insert(const char *, size_t);
-  void insert(const myString &, size_t);
-  myString substr(size_t, size_t) const;
-  void replace(const myString &, size_t, size_t);
-  void replace(const char *, size_t, size_t);
-  void clear();
-  size_t getSize() const;
-  bool empty() const;
-  char *c_str() const;
 
-  char &operator[](int64_t);
-  void operator=(const myString &);
-  void operator=(const char *);
-  bool operator==(myString &) const;
+/*========Конструкторы myString========*/
+    myString();
+    myString(int64_t);
+    myString(const myString &);
+    myString(myString &&);
+    myString(const char *);
+    ~myString();
 
-  myString operator+(const myString &) const;
 
-  friend std::ostream &operator<<(std::ostream &, myString &);
-  friend std::istream &operator>>(std::istream &, myString &);
+/*======Методы изменения myString======*/
+
+// Метод добавления символа в конец строки
+
+    void push_back(char);
+
+// Метод добавления подстрокиd к концу строки
+
+    void append(const char *);
+    void append(const myString &);
+
+// Метод вставки подстроки в строку
+
+    void insert(const char *, size_t);
+    void insert(const myString &, size_t);
+
+
+// Метод замены подстроки
+
+    void replace(const char *, size_t, size_t);
+    void replace(const myString &, size_t, size_t);
+
+// Метод очистки myString
+    void clear();
+
+/*===================Гетеры===================*/ 
+
+    bool empty() const;
+    char *c_str() const;
+    size_t getSize() const;
+    myString substr(size_t, size_t) const;
+
+
+/*===================Операторы===================*/
+   
+/*=======Операторы длля работы со строкой========*/
+
+    myString operator+(const myString &) const;
+    void operator=(const myString &);
+    char &operator[](size_t);
+
+/*==============Операторы сравнения==============*/  
+
+    bool operator==(const myString &) const;
+    bool operator!=(const myString &) const;
+    bool operator<(const myString &) const;
+    bool operator>(const myString &) const;
+    bool operator<=(const myString &) const;
+    bool operator>=(const myString &) const;
+   
+
+/*========Операторы приобразвания типов========*/
+
+    explicit operator std::string() const;
+    explicit operator int64_t() const;
+    explicit operator double() const;
+
+
+// Операторы ввода, вывода
+    friend std::ostream &operator<<(std::ostream &, myString &);
+    friend std::istream &operator>>(std::istream &, myString &);
+  
+
+
 };
 
 #endif // MYSTRING_HPP
